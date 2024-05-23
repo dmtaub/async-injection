5186a50d1f980bea6cf45b02ee2fb484fff8f710
Daniel Taub <dtaub@matterport.com>
2024-05-23 08:42:04 -0400

    use string keys for metadata

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,10 @@
-export const INJECTABLE_METADATA_KEY = Symbol('INJECTABLE_KEY');
-export const INJECT_METADATA_KEY = Symbol('INJECT_KEY');
-export const POSTCONSTRUCT_SYNC_METADATA_KEY = Symbol('POSTCONSTRUCT_SYNC_KEY');
-export const POSTCONSTRUCT_ASYNC_METADATA_KEY = Symbol('POSTCONSTRUCT_ASYNC_KEY');
-export const RELEASE_METADATA_KEY = Symbol('RELEASE_KEY');
-export const OPTIONAL_METADATA_KEY = Symbol('OPTIONAL_KEY');
+// string keys for access to metadata across compilation units
+export const INJECTABLE_METADATA_KEY = '__INJECTABLE_KEY__';
+export const INJECT_METADATA_KEY = '__INJECT_KEY__';
+export const POSTCONSTRUCT_SYNC_METADATA_KEY = '__POSTCONSTRUCT_SYNC_KEY__';
+export const POSTCONSTRUCT_ASYNC_METADATA_KEY = '__POSTCONSTRUCT_ASYNC_KEY__';
+export const RELEASE_METADATA_KEY = '__RELEASE_KEY__';
+export const OPTIONAL_METADATA_KEY = '__OPTIONAL_KEY__';
 
 export const REFLECT_PARAMS = 'design:paramtypes';
 export const REFLECT_RETURN = 'design:returntype';
